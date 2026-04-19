451749842eb59ebe7dea563e6ce6ffec46de4382
andreluizsantoss <andre.luiz.itape@gmail.com>
2026-04-19 20:23:41 -0300

    configuração do datasource do typeorm

---
+++ src/shared/infra/database/data-source.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { env } from '@shared/env';
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  username: env.DB_USER,
+  password: env.DB_PASS,
+  database: env.DB_NAME,
+  synchronize: false,
+  logging: env.NODE_ENV === 'development',
+  entities: ['src/modules/**/infra/typeorm/entities/*.ts'],
+  migrations: ['src/shared/infra/database/migrations/*.ts'],
+});
+
+export async function initializeDatabase(): Promise<void> {
+  await AppDataSource.initialize();
+}
