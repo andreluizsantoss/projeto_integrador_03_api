7c3825a58e4dfe7bd4ea1f43fc24bbe8e32cef39
andreluizsantoss <andre.luiz.itape@gmail.com>
2026-04-19 20:20:19 -0300

    adição do babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@shared': './src/shared',
+          '@modules': './src/modules',
+        },
+      },
+    ],
+  ],
+};
